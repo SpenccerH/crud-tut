da615a31c1d4f1e6f85aa2eb4fc5c5f390b2f699
SpenccerH <spencer.hum@cc.ca>
2024-12-19 09:39:45 -0500

    feat: frontend

---
+++ crud-frontend/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+    return (
+        <>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <a className="btn btn-ghost text-xl">CookKeeper</a>
+                </div>
+                <div className="navbar-center">
+                    <input type="text" placeholder="Search" className="input input-bordered w-48 md:w-auto" />
+                </div>
+                <div className="navbar-end">
+                    <a className="btn btn-primary">Add New</a>
+                </div>
+            </div>
+        </>
+    )
+}
